b5a6f7f646de186c0268043e89d2bf89da65a06e
hellosiri <46514419+hellosiri@users.noreply.github.com>
2019-11-09 15:31:47 +0800

    c

--- src/test/java/com/se/program/ProgramApplicationTests.java
+++ src/test/java/com/se/program/ProgramApplicationTests.java
@@ -7,8 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ProgramApplicationTests {
 
     @Test
-    void contextLoads() {
-        jkhgg
+    void contextLoads()
     }
 
 }
