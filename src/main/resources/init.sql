2e6b694558b25061fb6451389d5c91d96cd2d2c9
Denny <chengyaokai@126.com>
2019-12-08 11:03:17 +0800

    实体类 数据库配置完成

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -20,19 +20,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
-<<<<<<< HEAD
-                         `admin_id` int(30) NOT NULL,
-                         `admin_name` varchar(30) NOT NULL,
-                         `admin_permission` int(30) NOT NULL,
-                         `admin_password` varchar(30) NOT NULL,
-                         PRIMARY KEY (`admin_id`)
-=======
   `admin_id` int(30) NOT NULL,
   `admin_name` varchar(30) NOT NULL,
   `admin_permission` int(30) NOT NULL,
   `admin_password` varchar(30) NOT NULL,
   PRIMARY KEY (`admin_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -44,19 +36,13 @@ CREATE TABLE `admin` (
 -- ----------------------------
 DROP TABLE IF EXISTS `bill`;
 CREATE TABLE `bill` (
-<<<<<<< HEAD
-                        `bill_id` int(30) NOT NULL,
-                        `bill_image` varchar(30) NOT NULL,
-                        `form_id` int(5) NOT NULL,
-                        PRIMARY KEY (`bill_id`),
-                        KEY `form_id` (`form_id`)
-=======
   `bill_id` int(30) NOT NULL,
+  `bill_hospital` varchar(30) NOT NULL,
+  `bill_cost` int(30) NOT NULL,
   `bill_image` varchar(30) NOT NULL,
   `form_id` int(5) NOT NULL,
   PRIMARY KEY (`bill_id`),
   KEY `form_id` (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -68,19 +54,13 @@ CREATE TABLE `bill` (
 -- ----------------------------
 DROP TABLE IF EXISTS `changehospital`;
 CREATE TABLE `changehospital` (
-<<<<<<< HEAD
-                                  `changehospital_id` int(30) NOT NULL,
-                                  `changehospital_image` varchar(30) NOT NULL,
-                                  `form_id` int(30) NOT NULL,
-                                  PRIMARY KEY (`changehospital_id`),
-                                  KEY `form_id` (`form_id`)
-=======
   `changehospital_id` int(30) NOT NULL,
   `changehospital_image` varchar(30) NOT NULL,
+  `changehospital_in` varchar(30) NOT NULL,
+  `changehospital_out` varchar(30) NOT NULL,
   `form_id` int(30) NOT NULL,
   PRIMARY KEY (`changehospital_id`),
   KEY `form_id` (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -92,19 +72,11 @@ CREATE TABLE `changehospital` (
 -- ----------------------------
 DROP TABLE IF EXISTS `department`;
 CREATE TABLE `department` (
-<<<<<<< HEAD
-                              `department_id` int(30) NOT NULL DEFAULT '0',
-                              `department_oldname` varchar(30) NOT NULL,
-                              `department_name` varchar(30) DEFAULT NULL,
-                              `department_state` varchar(30) DEFAULT NULL,
-                              PRIMARY KEY (`department_id`)
-=======
   `department_id` int(30) NOT NULL DEFAULT '0',
   `department_oldname` varchar(30) NOT NULL,
   `department_name` varchar(30) DEFAULT NULL,
   `department_state` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`department_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -116,15 +88,6 @@ CREATE TABLE `department` (
 -- ----------------------------
 DROP TABLE IF EXISTS `form`;
 CREATE TABLE `form` (
-<<<<<<< HEAD
-                        `form_id` int(30) NOT NULL DEFAULT '0',
-                        `form_time` datetime NOT NULL,
-                        `form_kind` int(30) NOT NULL,
-                        `hospital` varchar(30) NOT NULL,
-                        `form_text` varchar(100) DEFAULT NULL,
-                        `hospital_department` varchar(30) NOT NULL,
-                        PRIMARY KEY (`form_id`)
-=======
   `form_id` int(30) NOT NULL DEFAULT '0',
   `form_time` datetime NOT NULL,
   `form_kind` int(30) NOT NULL,
@@ -132,7 +95,6 @@ CREATE TABLE `form` (
   `form_text` varchar(100) DEFAULT NULL,
   `hospital_department` varchar(30) NOT NULL,
   PRIMARY KEY (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -144,18 +106,6 @@ CREATE TABLE `form` (
 -- ----------------------------
 DROP TABLE IF EXISTS `operation`;
 CREATE TABLE `operation` (
-<<<<<<< HEAD
-                             `operation_id` int(30) NOT NULL,
-                             `form_id` int(30) NOT NULL,
-                             `operation_time` datetime NOT NULL,
-                             `amount` varchar(30) NOT NULL,
-                             `admin_id` int(30) NOT NULL,
-                             `operation_kind` int(30) NOT NULL,
-                             `operation_text` varchar(100) DEFAULT NULL,
-                             PRIMARY KEY (`operation_id`),
-                             KEY `admin_id` (`admin_id`),
-                             KEY `form_id` (`form_id`)
-=======
   `operation_id` int(30) NOT NULL,
   `form_id` int(30) NOT NULL,
   `operation_time` datetime NOT NULL,
@@ -166,7 +116,6 @@ CREATE TABLE `operation` (
   PRIMARY KEY (`operation_id`),
   KEY `admin_id` (`admin_id`),
   KEY `form_id` (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -178,19 +127,14 @@ CREATE TABLE `operation` (
 -- ----------------------------
 DROP TABLE IF EXISTS `prescription`;
 CREATE TABLE `prescription` (
-<<<<<<< HEAD
-                                `prescription_id` int(30) NOT NULL,
-                                `prescription_image` varchar(30) NOT NULL,
-                                `form_id` int(30) NOT NULL,
-                                PRIMARY KEY (`prescription_id`),
-                                KEY `form` (`form_id`)
-=======
   `prescription_id` int(30) NOT NULL,
+  `prescription_hospital` varchar(30) NOT NULL,
+  `prescription_department` varchar(30) NOT NULL,
+  `prescription_cost` int(30) NOT NULL,
   `prescription_image` varchar(30) NOT NULL,
   `form_id` int(30) NOT NULL,
   PRIMARY KEY (`prescription_id`),
   KEY `form` (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -202,16 +146,6 @@ CREATE TABLE `prescription` (
 -- ----------------------------
 DROP TABLE IF EXISTS `record`;
 CREATE TABLE `record` (
-<<<<<<< HEAD
-                          `record_id` int(11) NOT NULL,
-                          `user_id` int(11) NOT NULL,
-                          `record_time` datetime NOT NULL,
-                          `form_id` int(11) NOT NULL,
-                          `record_state` int(11) NOT NULL,
-                          PRIMARY KEY (`record_id`),
-                          KEY `user_id` (`user_id`),
-                          KEY `form_id` (`form_id`)
-=======
   `record_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `record_time` datetime NOT NULL,
@@ -220,7 +154,6 @@ CREATE TABLE `record` (
   PRIMARY KEY (`record_id`),
   KEY `user_id` (`user_id`),
   KEY `form_id` (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -232,19 +165,14 @@ CREATE TABLE `record` (
 -- ----------------------------
 DROP TABLE IF EXISTS `register`;
 CREATE TABLE `register` (
-<<<<<<< HEAD
-                            `register_id` int(30) NOT NULL,
-                            `register_image` varchar(30) NOT NULL,
-                            `form_id` int(30) NOT NULL,
-                            PRIMARY KEY (`register_id`),
-                            KEY `form_id` (`form_id`)
-=======
   `register_id` int(30) NOT NULL,
+  `register_hospital` varchar(30) NOT NULL,
+  `register_department` varchar(30) NOT NULL,
+  `register_cost` int(30) NOT NULL,
   `register_image` varchar(30) NOT NULL,
   `form_id` int(30) NOT NULL,
   PRIMARY KEY (`register_id`),
   KEY `form_id` (`form_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -256,21 +184,6 @@ CREATE TABLE `register` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-<<<<<<< HEAD
-                        `user_id` int(30) NOT NULL DEFAULT '0',
-                        `user_name` varchar(30) DEFAULT NULL,
-                        `user_sex` varchar(30) DEFAULT NULL,
-                        `user_idcard` varchar(30) DEFAULT NULL,
-                        `user_nation` varchar(30) DEFAULT NULL,
-                        `user_phone` varchar(30) DEFAULT NULL,
-                        `user_number` varchar(30) DEFAULT NULL,
-                        `user_password` varchar(30) DEFAULT NULL,
-                        `user_state` int(30) DEFAULT NULL,
-                        `user_type` int(30) DEFAULT NULL,
-                        `department_id` int(30) DEFAULT NULL,
-                        PRIMARY KEY (`user_id`),
-                        KEY `department_id` (`department_id`)
-=======
   `user_id` int(30) NOT NULL DEFAULT '0',
   `user_name` varchar(30) DEFAULT NULL,
   `user_sex` varchar(30) DEFAULT NULL,
@@ -279,17 +192,13 @@ CREATE TABLE `user` (
   `user_phone` varchar(30) DEFAULT NULL,
   `user_number` varchar(30) DEFAULT NULL,
   `user_password` varchar(30) DEFAULT NULL,
+  `user_type` int(30) DEFAULT NULL,
   `user_state` int(30) DEFAULT NULL,
   `department_id` int(30) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   KEY `department_id` (`department_id`)
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-<<<<<<< HEAD
-
-=======
->>>>>>> f20b9698318fbce204c0f794e30f405106f25b33
